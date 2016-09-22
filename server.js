2df91e0e302e0f654a3eb1d770fc1bdca9e62afb
Alex Reinking <alex_reinking@yahoo.com>
2016-09-21 19:01:07 -0500

    mongodb

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('./public'));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect('mongodb://admin:codingrocks@ds023664.mlab.com:23664/reactlocate');
+mongoose.connect('mongodb://heroku_32f5gfm0:ve5ujn47obfmlnev9h7qb6pro6@ds035776.mlab.com:35776/heroku_32f5gfm0');
 var db = mongoose.connection;
 
 db.on('error', function (err) {
